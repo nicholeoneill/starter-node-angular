31e5208b9938b1996a9b76745ea8a90b2f9995bf
Nichole ONeill <nazm612@gmail.com>
2021-08-13 11:16:04 -0700

    updating public IP address in deploy.js file

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.234.72.185',
+      host: '54.197.34.224',
       username: 'ubuntu',
       privateKey: 'final5-key.pem'
     })
